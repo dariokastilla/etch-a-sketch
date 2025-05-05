dfc10ea716f091b8ec2f9e9bffe81890863f8f1c
dariokastilla <dariokastillam@gmail.com>
2025-05-05 20:22:04 +0200

    Añadir tabla con prompt de pixeles byn

---
+++ javascript.js
@@ -0,0 +1,49 @@
+const container = document.getElementById("container");
+const boton = document.getElementById("boton");
+
+
+for(let i = 0; i < 16 * 16; i++){ //meto el for dentro
+    const div = document.createElement("div");
+    div.classList.add("cuadro");
+    container.appendChild(div);
+    
+
+    div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "black";
+    })
+}
+
+
+
+
+
+boton.addEventListener("click", () => {
+    let valor = prompt("Cuantos cuadrados quieres por lado? Máx. 100!");
+    let size = parseInt(valor);
+
+    if(size < 16 || size > 100 || isNaN(size)){
+        alert("Por favor, introduce un número entre 16 y 100");
+        return;
+    }
+
+    container.innerHTML = "";
+
+    for(let i = 0; i < size * size; i++){
+        //meto el for dentro
+        const div = document.createElement("div");
+        div.classList.add("cuadro");
+        div.style.width = `${480 / size}px`;
+        div.style.height = `${480 / size}px`;
+        container.appendChild(div);
+        
+    
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = "black";
+        })
+    }
+    
+})
+
+
+
+
